b4cd14bfd16b30f0f78465133c5798c96db5bec0
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-27 10:54:59 +0100

    typescript playground

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Test } from './test';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+
+// npm start
+const test = new Test(42);
+console.log(test.id);
