9219a04ab4a26564936713a8cde1cbb368f90d95
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-29 12:05:07 +0100

    final unit test

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,11 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-dashboard',
+  template: 'Hallo ich bin ein Dummy! 😃',
+  standalone: true
+})
+export class DummyDashboardComponent { }
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent], // Integration Test: DashboardComponent, BookComponent
-    }).compileComponents();
+    })
+    // Shallow Component Test
+    // .overrideComponent(AppComponent, {
+    //   set: { imports: [], schemas: [NO_ERRORS_SCHEMA] }
+    // })
+    // Unit Test
+    .overrideComponent(AppComponent, {
+      remove: { imports: [DashboardComponent] },
+      add: { imports: [DummyDashboardComponent] }
+    })
+    .compileComponents();
   });
 
   it(`should have the 'Book Rating' title`, () => {
