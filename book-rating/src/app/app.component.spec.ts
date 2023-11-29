b6f9a86f2afb73ad7d4c198e3652b7cbbde6be69
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-29 10:32:52 +0100

    fixing integration tests

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -4,26 +4,20 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent], // Integration Test: DashboardComponent, BookComponent
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it(`should have the 'Book Rating' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'book-rating' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-rating');
+    expect(app.title).toEqual('Book Rating');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-rating');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Book Rating');
   });
 });
