e39835f9eae4ce3f90ae19f195bf7eb0793d31d5
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-12-01 15:06:43 +0100

    rxjs: final example

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,13 +1,15 @@
-import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
+import { AsyncPipe, CommonModule, JsonPipe, NgIf } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { concatMap, map, switchMap } from 'rxjs';
+import { catchError, map, of, retry, switchMap } from 'rxjs';
+
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [CommonModule, AsyncPipe, JsonPipe, RouterLink],
+  imports: [CommonModule, AsyncPipe, JsonPipe, RouterLink, NgIf],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
@@ -17,7 +19,20 @@ export class BookDetailsComponent {
 
   book$ = inject(ActivatedRoute).paramMap.pipe(
     map(paramMap => paramMap.get('isbn') || ''),
-    switchMap(isbn => this.bookStore.getSingleBook(isbn))
-  );
+    switchMap(isbn => this.bookStore.getSingleBook(isbn).pipe(
+      retry({
+        count: 3,
+        delay: 1000
+      }),
+      catchError((err: HttpErrorResponse) => of({
+        isbn: '000',
+        title: 'FEHLER',
+        description: err.message,
+        rating: 0
+      })
+      )
+    )),
+    // shareReplay({ refCount: true, bufferSize: 1 })
+    );
 
 }
