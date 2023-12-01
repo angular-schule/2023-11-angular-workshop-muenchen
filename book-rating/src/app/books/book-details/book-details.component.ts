5d6d17cecf8c996613a952f4c1ce69c82eab3cf8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-12-01 13:59:35 +0100

    rxjs: WIP

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe, JsonPipe, RouterLink],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
+
+  book$ = inject(ActivatedRoute).paramMap.pipe(
+    map(paramMap => paramMap.get('isbn') || '')
+  );
 
 }
