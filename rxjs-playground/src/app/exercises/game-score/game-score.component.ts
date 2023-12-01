8167384490a47091820728d765a8ed22b6d0b632
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-12-01 11:30:02 +0100

    rxjs: scan + reduce

--- rxjs-playground/src/app/exercises/game-score/game-score.component.ts
+++ rxjs-playground/src/app/exercises/game-score/game-score.component.ts
@@ -23,7 +23,15 @@ export class GameScoreComponent {
 
     /******************************/
 
-    
+    this.score$.pipe(
+      scan((acc, item) => acc + item, 0)
+    ).subscribe(score => this.currentScore = score);
+
+    this.score$.pipe(
+      reduce((acc, item) => acc + item, 0)
+    ).subscribe(score => this.finalScore = score);
+
+
     /******************************/
 
     this.score$.subscribe({
